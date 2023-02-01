bff25046041a87080e63e4b70ed8cad7be1cb133
Vladislav Krivenko <vladkrivenko1995@yandex.ru>
2023-02-01 12:08:37 +0300

    done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "./App.css";
 import MainHeader from './components/MainHeader'
 import AboutMe from "./components/AboutMe";
+import ContactPage from "./components/ContactPage";
 
 function App() {
   return (
       <div>
           <MainHeader />
           <AboutMe />
+          <ContactPage />
       </div>
   );
 }
